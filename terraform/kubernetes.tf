18fdd32a700b043ecb9a7c6f4ffecb16abd8f5df
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-03-10 22:32:29 +0100

    rm duplicate k8s gsa service account

--- terraform/kubernetes.tf
+++
@@ -1,8 +0,0 @@
-resource "kubernetes_service_account" "gsa" {
-  metadata {
-    name = var.gsa_name
-    annotations = {
-      "iam.gke.io/gcp-service-account" = "${var.gsa_name}@${var.gcp_project}.iam.gserviceaccount.com"
-    }
-  }
-}
