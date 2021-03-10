a66bba423286cf67490d9f37c5579adb046923f5
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-03-10 22:31:54 +0100

    mv cloudsql + create cloudsql user

---
+++ terraform/cloudsql.tf
@@ -0,0 +1,19 @@
+resource "google_sql_database_instance" "master" {
+  name             = "${var.app}-db"
+  database_version = "POSTGRES_${var.db_version}"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_user" "user" {
+  depends_on = [
+    "google_sql_database_instance.master"
+  ]
+
+  instance = google_sql_database_instance.master.name
+  name     = var.cloudsql_user
+  password = var.cloudsql_password
+}
