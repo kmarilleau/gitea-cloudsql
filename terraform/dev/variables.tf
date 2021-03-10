b81e9207bea3e9258da089b3ee7af91308b5c8b1
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-03-10 16:31:42 +0100

    add terraform

---
+++ terraform/dev/variables.tf
@@ -0,0 +1,15 @@
+variable "app" {
+  default = "gitea-cloudsql"
+}
+
+variable "gcp_project" {
+  type = string
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
