116f4401093cfce76aac1d8a6740bcf0edddfade
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-03-10 22:31:33 +0100

    add k8s_ressources gitea

---
+++ terraform/k8s_ressources.tf
@@ -0,0 +1,58 @@
+resource "kubernetes_service_account" "gsa" {
+  metadata {
+    name = var.gsa_name
+    annotations = {
+      "iam.gke.io/gcp-service-account" = "${var.gsa_name}@${var.gcp_project}.iam.gserviceaccount.com"
+    }
+  }
+}
+
+resource "kubernetes_deployment" "gitea" {
+  metadata {
+    name = "gitea"
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "gitea"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "gitea"
+        }
+      }
+
+      spec {
+        container {
+          name  = "gitea"
+          image = "gitea/gitea:1.13.4"
+
+          port {
+            container_port = 3000
+          }
+        }
+
+        container {
+          name  = "cloudsql-proxy"
+          image = "gcr.io/cloudsql-docker/gce-proxy:1.20.2"
+          command = [
+            "/cloud_sql_proxy",
+            "-instances=${var.gcp_project}:${var.region}:${google_sql_database_instance.master.name}=tcp:5432"
+          ]
+
+          security_context {
+            run_as_non_root = true
+          }
+        }
+
+        restart_policy       = "Always"
+        service_account_name = var.gsa_name
+      }
+    }
+  }
+}
+
