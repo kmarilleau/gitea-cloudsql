bd97a5502061080850941f20043b751b0763f4c6
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-03-10 20:49:46 +0100

    add k8s cluster provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,20 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
+
+data "google_client_config" "provider" {}
+
+data "google_container_cluster" "cluster" {
+  name     = var.app
+  location = var.region
+}
+
+provider "kubernetes" {
+  load_config_file = false
+
+  host  = "https://${data.google_container_cluster.cluster.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
